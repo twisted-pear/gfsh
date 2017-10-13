129ac1b163dfc5c2274e4841a4363046a79cfd0b
twisted_pear <twisted_pear@twstd.neomailbox.ch>
2017-10-13 19:56:24 +0200

    Factor our C string array functions

--- run.fs
+++ run.fs
@@ -1,42 +1,8 @@
 require cfuncs.fs
+require cstrarrays.fs
 require fd.fs
 require lib.fs
 require builtins.fs
-
-: free-argv ( a-addr -- )
-	dup begin
-		dup @ dup while
-			free drop
-			cell+
-	repeat 2drop
-	free drop ;
-
-: store-arg ( c-addr u a-addr -- )
-	rot rot >c-string swap ! ;
-
-: store-args ( c-addr1 u1 c-addr2 u2... a-addr u -- )
-	dup >r over >r 0> if
-		store-arg r> cell+ r> 1- recurse
-	else
-		r> r> 2drop drop
-	endif ;
-
-: alloc-argv ( u -- a-addr )
-	1+ cells dup allocate throw
-	dup rot erase ;
-
-: init-argv ( c-addr1 u1 c-addr2 u2... u -- a-addr )
-	dup alloc-argv >r r@ swap
-	['] store-args catch dup if
-		r> free-argv
-	endif throw r> ;
-
-: consume-argv ( c-addr1 u1 c-addr2 u2... u -- )
-	dup 0> if
-		1- >r 2drop r> recurse
-	else
-		drop
-	endif ;
 
 : child-exec ( c-addr1 u1 c-addr2 u2 ... u c-addrN uN -- n )
 	rot 1+
